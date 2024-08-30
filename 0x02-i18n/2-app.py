6efcb2f942db29a832e86863ec3ed8e6717ae599
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-08-30 15:22:45 +0100

    added localeselector decorator

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -6,6 +6,7 @@ Basic Flask app with Babel for timezone and location
 from flask import Flask, render_template, request
 from babel import Babel, localeselector
 
+
 app = Flask(__name__)
 
 
@@ -19,15 +20,17 @@ app.config.from_object(Config)
 
 babel = Babel(app)
 
-@babel_localeselector
+@babel.localeselector
 def get_locale():
     """ Select best match language """
     return request.accept_languages.best_match(['en', 'fr'])
+
 
 @app.route('/', strict_slashes=False)
 def basic():
     """ render root path """
     return render_template('2-index.html')
+
 
 if __name__ == '__main__':
     """ Run app """
